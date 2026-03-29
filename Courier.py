37cc33d4e932940d77ef90255307ef784fbd72f3
SILVERSTONE <dise209@gmail.com>
2026-03-29 16:45:28 +0300

    7

--- Courier.py
+++ Courier.py
@@ -1,6 +1,6 @@
 ﻿class Courier:
     name: str
-    experiance:int
+    experiance: int
     rating: float
     completed_orders: int
     balance: float
@@ -13,18 +13,21 @@
         self.completed_orders = completed_orders
         self.balance = balance
         self.is_busy = is_busy
+    
     def print_info(self):
-        print(f"Name: {self.name}")
-        print(f"Experience: {self.experiance} years")
-        print(f"Rating: {self.rating}")
-        print(f"Completed Orders: {self.completed_orders}")
-        print(f"Balance: ")
-        print(f"Is Busy: {self.is_busy}")
+        print(f'Name: {self.name}')
+        print(f'Experience: {self.experiance} years')
+        print(f'Rating: {self.rating}')
+        print(f'Completed Orders: {self.completed_orders}')
+        print(f'Balance: {self.balance}')
+        print(f'Is Busy: {self.is_busy}')
+    
     def deliver_order(self, distance: float):
         if distance <= 0:
-            raise NotImplementedError("Distance must be greater than zero.")
+            raise NotImplementedError('Distance must be greater than zero.')
         else:
             self.distance = distance
+    
     def finish_shift(self):
         self.is_busy = False
-        print(f"{self.completed_orders} order completed. Balance: . Shift finished.")
+        print(f'{self.completed_orders} order completed. Balance: {self.balance}. Shift finished.')
